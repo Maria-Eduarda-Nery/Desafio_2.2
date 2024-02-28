93b988ad9af4e8f4284b0ea5fec17b37b0cdb391
Maria-Eduarda-Nery <142039771+Maria-Eduarda-Nery@users.noreply.github.com>
2024-02-28 15:38:03 -0300

    teste

---
+++ desafio-2.2/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Componentes/Header/Header.jsx";
+
+function App (){
+return (
+  <>
+  <Header/> 
+  <h1>Mundo Otaku</h1>
+  </>
+  
+  )
+
+}
+
+export default App
---
+++ desafio-2.2/src/Componentes/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header (){
+    return (
+        <Header>
+            <nav>
+                <form action="PESQUISA"></form>
+                <ul>animes disponiveis</ul>
+                <ul>tipode objetos</ul>
+                
+            </nav>
+        </Header>
+        
+    )
+}
+
+export default Header
